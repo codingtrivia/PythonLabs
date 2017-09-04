5fabb35daac56839208118bfb870914f5eea0a83
codingtrivia <codingtrivia@gmail.com>
2017-09-04 16:34:54 -0400

    Lab1

---
+++ Lab1/lab1.py
@@ -0,0 +1,12 @@
+# 1.
+# Create test.py with following contents and run this file using python from command prompt like discussed in class.
+print("Start Program")
+for i in range(100):
+    print(i)
+print("End Program")
+
+# 2.
+# Add code in below program to print 0 to 100 10 times. Enter code where "?" is present
+for i in range(10):
+    for j in range(?):
+        ?
