56b3b8df5111d03413e0b3ada9627e4be3e50d93
codingtrivia <codingtrivia@gmail.com>
2017-09-05 19:06:29 -0400

    Lab2

---
+++ Lab2/lab2.py
@@ -0,0 +1,30 @@
+a = 5
+b = 2
+
+# 1.  how will i get value 2.5 on division of a/b in 2 ways hint: int to float
+# 2.  how will i get 2 on division of a/b
+# 3.  how will i get 25 i.e. 5^2 from a and b
+# 4.  how will i get decimal value of bits '101'
+a = ['aaa']
+b = ['bbb', "ccc"]
+# 5. after applying what operation on a can i get a list ['aaa', 'aaa', 'aaa']
+# 6. after applying what operation on a and b can i get a list ['bbb', 'ccc', 'aaa']
+# 7. How can i find length of list a
+# 8. how can i get largest (hint max) value from list b i.e. ccc
+# 9. how can i get smallest value from list b i.e. bbb
+# 10. how can i slice list b so that i get back list ['ccc']
+# 11. how can i find get back value 'ccc' using index operation on list provide 2 ways of doing it.. hint -1
+# 12. using for loop print as follows from list b
+# " this is value bbb"
+# " this is value ccc"
+
+a = {
+    "naomi": [10, 'female'],
+    "john": [15, 'male']
+}
+# 13. using indexing on dictionary how to print value [10, 'female']
+# 14. Using for loop print all values in dictionary a
+# 15. using for loop print all key and values in dictionary a
+
+
+# 16. If i print all keys in dictionary using for loop, will they come in order or not?
