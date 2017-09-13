9cdc066b8630b2f9731918b58ba526d7d668c0fb
codingtrivia <codingtrivia@gmail.com>
2017-09-13 19:10:58 -0400

    Lab5

---
+++ Lab5/Lab5.py
@@ -0,0 +1,5 @@
+# Question 1: Write a program that defines a function: average and that function calculates average of numbers from 0 to 100
+
+# Question 2: We have a dictionary as follows.
+nameAndAge = {"Joe": 18, "Katie": 21, "John": 23, "Jack": 24}
+# How would you print entries (or items) from the above dictionary where the names (keys) start with J
