0cda3538b581d2d75640355d0f1b1ee04a737406
Sumit Bahl <codingtrivia>
2017-10-24 21:36:17 -0400

    Intermediate Lab3

---
+++ Intermediate/Lab3/Lab3.py
@@ -0,0 +1,14 @@
+# Using recursion, print only even numbers in descending order. For e.g. if I say, print_down(10), output would be:
+# 10
+# 8
+# 6
+# 4
+# 2
+
+# You will need to do a slight modification from the code we wrote for print_up_down in our class today.
+
+def print_down(n):
+    #<your code here>
+
+
+print_down(10)
